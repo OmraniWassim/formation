6002923d5c9cc81cfdc1dddc6f3dafe8d3757897
OmraniWassim <omraniwassim1@essths.u-sousse.tn>
2023-11-20 06:31:27 +0100

    hello to work

--- src/main/java/com/grh/formation/FormationApplication.java
+++ src/main/java/com/grh/formation/FormationApplication.java
@@ -3,17 +3,13 @@ package com.grh.formation;
 import com.grh.formation.model.*;
 import com.grh.formation.repo.*;
 import com.grh.formation.service.ContractTypeService;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.yaml.snakeyaml.events.Event;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class FormationApplication {
@@ -22,22 +18,8 @@ public class FormationApplication {
 
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(FormationApplication.class, args);
-	}
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		var corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList( "http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter();
 	}
 	@Bean
 	CommandLineRunner run(EtudeLevelRepo etudeLevelRepo,
